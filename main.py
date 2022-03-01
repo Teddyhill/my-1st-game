ac4cc1d68a71a679691a60278af472031ce6a733
Teddyhill <98024074+Teddyhill@users.noreply.github.com>
2022-03-01 14:34:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
+from re import X
+from turtle import Screen
 import pygame
-#import pygame.freetype
-# screen size 
-WINDOW_W = 837
-WINDOW_H = 478 
+
+
+WINDOW_W = 1000
+WINDOW_H = 641
 WINDOW_SIZE = (WINDOW_W, WINDOW_H)
 pygame.init()
+
 
 screen = pygame.display.set_mode(WINDOW_SIZE)
 square = pygame.image.load("pngaaa.com-472793.png")
 square = pygame.transform.scale(square,(50,90))
-screen.fill(screen(0,0,0))
 
-#clock = pygame.time.Clock()
 
-circle_x = 10
-circle_y = WINDOW_H /2
-ship_x = WINDOW_W /2
-ship_y = WINDOW_H - 80
-circle_x_step = 10
-x_step = 10
-laser_list = []
 play = True
 
-
-# laser_list = [[121,780],[171,780]]
-# i=1
-# l=[171,10]
-# l[0] = 171
 while play:
-  screen.blit(square,(0,0))
-
+  screen.fill((0,0,0))
+  screen.blit(square,(500,300))
   for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
+  x = 250
+  for i in range(7):
+     pygame.draw.line(screen, (255,0,0), (x, 0), (x, 1000),4)
+     x += 100
 
   pygame.display.flip()
- # clock.tick(10)
+
 pygame.quit()
